e6e742ce4e85425a2221dfb199f1b55b215358f7
gabrielj <gabriel.ggcj@gmail.com>
2023-09-28 14:39:18 -0300

    feature/add-default-layout

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/Home";
 import { CompletedOrderPage } from "./pages/completeOrder";
+import { DefaultLayout } from "./layouts/DefaltLayout";
 
 export function Router() {
   return (
     <Routes>
-        <Route path="/" element={<HomePage />} />        
-        <Route path="/completedOrder" element={<CompletedOrderPage />} />        
+        <Route path="/" element={<DefaultLayout />}>
+            <Route path="/" element={<HomePage />} />        
+            <Route path="/completedOrder" element={<CompletedOrderPage />} />        
+        </Route>
     </Routes>
   );
 }
