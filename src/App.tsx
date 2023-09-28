d3f2e91bc29ff924c7c54408e21c1b12addd2e92
gabrielj <gabriel.ggcj@gmail.com>
2023-09-28 14:28:05 -0300

    feature/add-routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./styles/theme/default"
 import { GlobalStyle } from "./styles/global"
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from "./Routes"
 
 export const App = () => {
 
   return (
     <ThemeProvider theme={defaultTheme}> 
     <GlobalStyle />
-    <h1>ola</h1>
+      <BrowserRouter>
+        <Router/> 
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
